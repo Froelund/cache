ed34da263558c27b6d2b9f3e38e9fbd46397ac4c
Kristian Frølund <kristian@froelund.net>
2014-04-07 21:48:12 +0200

    Initial commit.

---
+++ src/main/java/net/froelund/Cache.java
@@ -0,0 +1,34 @@
+package net.froelund;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.ConcurrencyManagementType;
+import javax.ejb.Singleton;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by Froelund on 4/7/14.
+ */
+@Singleton
+@ConcurrencyManagement(ConcurrencyManagementType.BEAN)
+public class Cache {
+
+    ConcurrentHashMap cachedObjects = null;
+
+    @PostConstruct
+    public void initialize(){
+
+    }
+    public void put(String cacheKey, Object cached){
+        cachedObjects.put(cacheKey, cached);
+    }
+    public Object get(String cacheKey){
+        return cachedObjects.get(cacheKey);
+    }
+    public boolean has(String cacheKey){
+        return cachedObjects.containsKey(cacheKey);
+    }
+    public void delete(String cacheKey){
+        cachedObjects.remove(cacheKey);
+    }
+}
---
+++ src/test/java/net/froelund/CacheTest.java
@@ -0,0 +1,53 @@
+package net.froelund;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by Froelund on 4/7/14.
+ */
+public class CacheTest {
+
+    Cache cache;
+
+    @Before
+    public void setUp() throws Exception {
+        cache = new Cache();
+        //As @PostConstruct never is run.
+        cache.cachedObjects = new ConcurrentHashMap();
+    }
+    @Test
+    public void testSingleObject(){
+        String cacheKey = "MY_TEST_KEY";
+        String cached = "MY TEST VALUE";
+        cache.put(cacheKey, cached);
+        Assert.assertEquals("Single object put, wasn't the same as the one get.", cached, cache.get(cacheKey));
+    }
+    @Test
+    public void testObjectOverwrite(){
+        String cacheKey = "MY_TEST_KEY";
+        String initialCachedObject = "MY TEST VALUE";
+        String newCachedObject = "MY NEW TEST VALUE";
+        cache.put(cacheKey, initialCachedObject);
+        cache.put(cacheKey, newCachedObject);
+        Assert.assertEquals("Overwrite of object, doesn't work.", newCachedObject, cache.get(cacheKey));
+    }
+    @Test
+    public void testObjectDelete(){
+        String cacheKey = "MY_TEST_KEY";
+        String cached = "MY TEST VALUE";
+        cache.put(cacheKey, cached);
+        cache.delete(cacheKey);
+        Assert.assertNull("Object wasn't deleted.", cache.get(cacheKey));
+    }
+    @Test
+    public void testObjectExistence(){
+        String cacheKey = "MY_TEST_KEY";
+        String cached = "MY TEST VALUE";
+        cache.put(cacheKey, cached);
+        Assert.assertTrue("Object wasn't in cache.", cache.has(cacheKey));
+    }
+}
